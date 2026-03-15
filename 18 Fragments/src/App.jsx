ffcbe9e5e0b228e546bc7766d0b8bcada403b4a2
KH Bidyut <khbidyut31@gmail.com>
2026-03-14 22:13:15 -0700

    learn map and condition

--- 18 Fragments/src/App.jsx
+++ 18 Fragments/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+
 
 const App = () => {
+
+  // let foodItem = ['dal','green vegrafd', 'adfa', 'adflkja', 'akdfla']
+  let foodItem = []
+  let emptyMessage = foodItem.length === 5 ? <h>full</h>: null;
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <h1>Healthy Food</h1>
+      {foodItem.length === 0 && <h3>ki shob bolo kili</h3>}
+      {emptyMessage}
+      {foodItem.length ===0 ? <h3>I am still hungry.</h3>: null}
+      <ul className="list-group">
+        {foodItem.map((items,index) =><li key={index} class="list-group-item">{items}</li>)}
+        
+      </ul>
+    </>
   );
 };
 
