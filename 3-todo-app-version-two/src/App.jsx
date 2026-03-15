92c30835954f1281836c45ef367fa6e1546dc0b1
KH Bidyut <khbidyut31@gmail.com>
2026-03-15 10:38:25 -0700

    start todo-app-version-two

---
+++ 3-todo-app-version-two/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppName from './components/AppName';
+import AddTodo from './components/AddTodo';
+import TodoItem1 from './components/TodoItem1';
+import TodoItem2 from './components/TodoItem2';
+import './App.css'
+const App = () => {
+  return (
+    <div className="todo-container text-center">
+      <AppName />
+      <div className="items-container">
+        <AddTodo />
+        <TodoItem1 />
+        <TodoItem2 />
+      </div>
+    </div>
+  );
+};
+
+export default App;
