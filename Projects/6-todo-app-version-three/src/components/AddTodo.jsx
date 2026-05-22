8a7539533dee09bc9ccb468070e09f1c6260f023
KH Bidyut <khbidyut31@gmail.com>
2026-05-22 17:04:47 +0600

    refactor AddTodo component to use refs for input handling and clean up commented code

--- Projects/6-todo-app-version-three/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -1,50 +1,61 @@
-import React, { useState } from "react";
+import React, {  useRef } from "react";
 
 const AddTodo = ({ onNewItems }) => {
-  const [todoName, setTodoName] = useState('');
-  const [dueDate, setDueDate] = useState('');
+  // const [todoName, setTodoName] = useState('');
+  // const [dueDate, setDueDate] = useState('');
+  const todoNameElements = useRef();
+  const dueDateElements = useRef();
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-    console.log(event.target.value);
-    
-  };
-  
-  // comment 
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-    console.log(event.target.value);
-  };
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
 
-  const handleAddButtonClick = () =>{
-    onNewItems(todoName, dueDate)
-    setTodoName('')
-    setDueDate('')
-  }
+  // };
+
+  // comment
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+
+  const handleAddButtonClick = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElements.current.value;
+    const dueDate = dueDateElements.current.value;
+    console.log(todoName + " & " + dueDate);
+    todoNameElements.current.value = "";
+    dueDateElements.current.value = "";
+    onNewItems(todoName, dueDate);
+
+    // onNewItems(todoName, dueDate)
+    // setTodoName('')
+    // setDueDate('')
+  };
 
   return (
     <div className="container">
-      <div className="row">
+      <form className="row" onSubmit={handleAddButtonClick}>
         <div className="col-6">
           <input
+            ref={todoNameElements}
             type="text"
             placeholder="Enter Todo Here"
-            value={todoName}
-            onChange={handleNameChange}
+            // value={todoName}
+            // onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" onChange={handleDateChange} value={dueDate} />
+          <input
+            type="date"
+            ref={dueDateElements}
+            // onChange={handleDateChange}
+            // value={dueDate}
+          />
         </div>
         <div className="col-2">
-          <button
-            className="btn btn-success kg-button"
-            onClick={handleAddButtonClick}
-          >
+          <button className="btn btn-success kg-button" type="submit">
             Add
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
