2f1b9103dd3d2a2ab1803bddab1da9caf633cfaf
KH Bidyut <khbidyut31@gmail.com>
2026-06-11 09:53:21 +0600

    start new social-media project

---
+++ Projects/7-social-media/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Sidebar></Sidebar>
+    </>
+  );
+};
+
+export default App;
