2f0950cdead12301ab8d4f3b620a6c9e95cb6629
KH Bidyut <khbidyut31@gmail.com>
2026-06-15 22:12:48 +0600

    implement delete functionality in PostList context; update Post component to handle post deletion and display reactions

--- Projects/7-social-media/src/components/Post.jsx
+++ Projects/7-social-media/src/components/Post.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MdDelete } from "react-icons/md";
+import { PostList } from "../store/PostListStore";
 
 const Post = ({ post }) => {
+  const {deletePost} = useContext(PostList);
   return (
-    <div className="card post-card" style={{ width: "18rem" }}>
+    <div className="card post-card" style={{ width: "30rem" }}>
       <div className="card-body">
         <h5 className="card-title">
           {post.title}
-          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-            {post.reaction}
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" onClick={()=>deletePost(post.id)}>
+            <MdDelete />
           </span>
         </h5>
 
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
-          <span class="badge text-bg-primary hashtag">{tag}</span>
+          <span className="badge text-bg-primary hashtag" key={tag}>{tag}</span>
         ))}
+        <div className="alert alert-success reaction" role="alert">
+          This post has been reacted by {post.reaction} people
+        </div>
       </div>
     </div>
   );
