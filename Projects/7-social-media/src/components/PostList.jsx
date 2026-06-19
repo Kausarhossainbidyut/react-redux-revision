d649c2c963c93fbe77e2eef6a9c124a6414aa2a8
KH Bidyut <khbidyut31@gmail.com>
2026-06-19 16:11:27 +0600

    refactor PostList component; streamline data fetching logic using useEffect and remove unused state management

--- Projects/7-social-media/src/components/PostList.jsx
+++ Projects/7-social-media/src/components/PostList.jsx
@@ -1,21 +1,31 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/PostListStore";
 import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList,addInitialPosts } = useContext(PostListData);
+  const { postList, addInitialPosts } = useContext(PostListData);
 
-  const [dataFetched, setDataFetched] = useState(false)
-
-  if(!dataFetched){
+  // 3rd workspace
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then((data) =>{
-        addInitialPosts(data.posts)
-      })
-      setDataFetched(true)
-  }
+        .then((res) => res.json())
+        .then((data) =>{
+          addInitialPosts(data.posts)
+        })
+  },[]);
+
+  // 2nd workspace
+  // const [dataFetched, setDataFetched] = useState(false)
+
+  // if(!dataFetched){
+  //   fetch("https://dummyjson.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) =>{
+  //       addInitialPosts(data.posts)
+  //     })
+  //     setDataFetched(true)
+  // }
 
   // 1st workspace
   // const handleGetPostsClick = () => {
@@ -26,12 +36,9 @@ const PostList = () => {
   //     })
   // }
 
-
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessage  />
-      )}
+      {postList.length === 0 && <WelcomeMessage />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
