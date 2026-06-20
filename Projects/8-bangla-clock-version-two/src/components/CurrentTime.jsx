1558f451c54329b6dae6316ade87685928baa07e
KH Bidyut <khbidyut31@gmail.com>
2026-06-20 14:54:15 +0600

    refactor CurrentTime component; implement state management for real-time clock updates using useEffect

--- Projects/8-bangla-clock-version-two/src/components/CurrentTime.jsx
+++ Projects/8-bangla-clock-version-two/src/components/CurrentTime.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CurrentTime = () => {
-    let time = new Date();
+    const [time, setTime] =useState(new Date())
+
+    useEffect(()=>{
+        const intervalId = setInterval(()=>{
+            setTime(new Date())
+        }, 1000)
+
+        return () =>{
+            clearInterval(intervalId)
+            
+        }
+    },[])
     return (
         <p className='lead'>This is the current time: {time.toLocaleString()} </p>
     );
