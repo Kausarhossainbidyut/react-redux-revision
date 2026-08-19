575fd228d9500fbaee9cad758f168062bdeac08f
Md. Kausar Hossain Bidyut <khbidyut31@gmail.com>
2026-08-19 11:00:56 +0600

    feat(Countrols): add increment, decrement, and add functionality with buttons fix(index.js): ensure proper formatting and add newline at end of file

--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,13 +1,20 @@
 import { createStore } from "redux";
 
-const INITIAL_VALUE ={
-    counter: 0
-}
+const INITIAL_VALUE = {
+  counter: 0,
+};
 
 const counterReducer = (store = INITIAL_VALUE, action) => {
+  if (action.type === "INCREMENT") {
+    return { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { counter: store.counter - 1 };
+  } else if (action.type === "ADD") {
+    return { counter: store.counter + action.payload.num };
+  }
   return store;
 };
 
 const counterStore = createStore(counterReducer);
 
-export default counterStore
+export default counterStore;
